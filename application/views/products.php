c248a8ac7adf59d40ab4ca7236a62a5c06216f33
Kava Developers <mehul9921@gmail.com>
2020-08-30 11:21:06 +0530

    Update products.php

--- application/views/products.php
+++ application/views/products.php
@@ -40,6 +40,7 @@
 		$('#dtAjax').DataTable({
 			"pageLength" : 10,
 			"serverSide": true,
+			"processing": true,
 			"language": {
                 processing: '<i class="fa fa-spinner fa-spin fa-3x fa-fw"></i><span class="sr-only">Loading...</span>'
             }, 
